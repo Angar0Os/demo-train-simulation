d20f9a13925c9f98dde03cea7e83ed887d3f6b60
fra <fra@movida-mail.com>
2024-10-28 11:59:19 +0100

    First batch of assets.

--- app/main.lua
+++ app/main.lua
@@ -15,12 +15,17 @@ res = hg.PipelineResources()
 
 -- load scene
 scene = hg.Scene()
-hg.LoadSceneFromAssets("main.scn", scene, res, hg.GetForwardPipelineInfo())
+hg.LoadSceneFromAssets("Demo_scene.scn", scene, res, hg.GetForwardPipelineInfo())
 
 -- AAA pipeline
 pipeline_aaa_config = hg.ForwardPipelineAAAConfig()
-pipeline_aaa = hg.CreateForwardPipelineAAAFromAssets("core", pipeline_aaa_config, hg.BR_Equal, hg.BR_Equal)
+pipeline_aaa = hg.CreateForwardPipelineAAAFromAssets("core", pipeline_aaa_config, hg.BR_Half, hg.BR_Half)
 pipeline_aaa_config.sample_count = 1
+pipeline_aaa_config.motion_blur = 0.001
+pipeline_aaa_config.exposure = 1.2
+pipeline_aaa_config.gamma = 1.8
+pipeline_aaa_config.z_thickness  = 0.25
+pipeline_aaa_config.sharpen = 0.25
 
 local camera_node = scene:GetNode("Camera")
 local cam_pos = camera_node:GetTransform():GetPos()
@@ -34,14 +39,15 @@ while not hg.ReadKeyboard():Key(hg.K_Escape) and hg.IsWindowOpen(win) do
 
 	-- trs = scene:GetNode('engine_master'):GetTransform()
 	-- trs:SetRot(trs:GetRot() + hg.Vec3(0, hg.Deg(15) * hg.time_to_sec_f(dt), 0))
-	cam_pos.z = cam_pos.z + hg.time_to_sec_f(dt) * speed
+	cam_pos.x = cam_pos.x + hg.time_to_sec_f(dt) * speed
 	camera_node:GetTransform():SetPos(cam_pos)
 
-	if cam_pos.z > 200.0 then
-		cam_pos.z = cam_pos.z - 100.0
+	if cam_pos.x > 200.0 then
+		cam_pos.x = cam_pos.x - 100.0
 	end
 
 	scene:Update(dt)
+	-- hg.SubmitSceneToPipeline(0, scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res)
 	hg.SubmitSceneToPipeline(0, scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res, pipeline_aaa, pipeline_aaa_config, frame)
 
 	frame = hg.Frame()
