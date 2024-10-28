ee40d77bf82425aac63fefebd1782caf6a5809ff
fra <astrofra@gmail.com>
2024-10-28 20:57:06 +0100

    Beauty pass.

--- app/main.lua
+++ app/main.lua
@@ -3,39 +3,44 @@ hg = require("harfang")
 hg.InputInit()
 hg.WindowSystemInit()
 
-res_x, res_y = 1280, 720
-win = hg.RenderInit('AAA Scene', res_x, res_y, hg.RF_VSync | hg.RF_MSAA4X)
+res_x, res_y = 1920, 1080 -- 1280, 720
+win = hg.RenderInit('Train Simulator', res_x, res_y, hg.RF_VSync) -- | hg.RF_MSAA4X)
 
 hg.AddAssetsFolder("assets_compiled")
 
 --
-pipeline = hg.CreateForwardPipeline()
+pipeline = hg.CreateForwardPipeline(4096, false)
 res = hg.PipelineResources()
 
 max_len = 4967.0 -- in meters
 
-
--- load scene
-scene = hg.Scene()
-hg.LoadSceneFromAssets("camera.scn", scene, res, hg.GetForwardPipelineInfo())
-hg.LoadSceneFromAssets("demo_scene.scn", scene, res, hg.GetForwardPipelineInfo())
+-- load main scene
+main_scene = hg.Scene()
+hg.LoadSceneFromAssets("camera.scn", main_scene, res, hg.GetForwardPipelineInfo())
+hg.LoadSceneFromAssets("skybox_day.scn", main_scene, res, hg.GetForwardPipelineInfo())
+hg.LoadSceneFromAssets("demo_scene.scn", main_scene, res, hg.GetForwardPipelineInfo())
 
 -- AAA pipeline
 pipeline_aaa_config = hg.ForwardPipelineAAAConfig()
 pipeline_aaa = hg.CreateForwardPipelineAAAFromAssets("core", pipeline_aaa_config, hg.BR_Equal, hg.BR_Equal)
+
 pipeline_aaa_config.sample_count = 1
 pipeline_aaa_config.motion_blur = 0.001
-pipeline_aaa_config.exposure = 1.2
-pipeline_aaa_config.gamma = 1.8
-pipeline_aaa_config.z_thickness  = 0.1
-pipeline_aaa_config.sharpen = 0.25
+pipeline_aaa_config.exposure = 1.4
+pipeline_aaa_config.gamma = 1.6
+pipeline_aaa_config.z_thickness  = 1.0
+pipeline_aaa_config.sharpen = 0.5
 
-local camera_node = scene:GetNode("RenderCamera")
-local cam_pos = camera_node:GetTransform():GetPos()
+-- Set camera
+local main_camera_node = main_scene:GetNode("RenderCamera")
+local cam_pos = main_camera_node:GetTransform():GetPos()
 cam_pos.z = cam_pos.z + 1.8
 local speed = 5.0
 
-scene:SetCurrentCamera(camera_node)
+main_scene:SetCurrentCamera(main_camera_node)
+
+local skybox_node = main_scene:GetNode("skydome")
+local skybox_pos = skybox_node:GetTransform():GetPos()
 
 -- main loop
 frame = 0
@@ -43,18 +48,25 @@ frame = 0
 while not hg.ReadKeyboard():Key(hg.K_Escape) and hg.IsWindowOpen(win) do
 	dt = hg.TickClock()
 
-	-- trs = scene:GetNode('engine_master'):GetTransform()
+	local view_id = 0
+	local pass_id
+
+	-- trs = main_scene:GetNode('engine_master'):GetTransform()
 	-- trs:SetRot(trs:GetRot() + hg.Vec3(0, hg.Deg(15) * hg.time_to_sec_f(dt), 0))
 	cam_pos.x = cam_pos.x + hg.time_to_sec_f(dt) * speed
-	camera_node:GetTransform():SetPos(cam_pos)
+	main_camera_node:GetTransform():SetPos(cam_pos)
+	skybox_pos.x = cam_pos.x
+	skybox_node:GetTransform():SetPos(skybox_pos)
 
 	if cam_pos.x > max_len then
 		cam_pos.x = cam_pos.x - max_len
 	end
 
-	scene:Update(dt)
-	-- hg.SubmitSceneToPipeline(0, scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res)
-	hg.SubmitSceneToPipeline(0, scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res, pipeline_aaa, pipeline_aaa_config, frame)
+	main_scene:Update(dt)
+
+	-- render main scene
+	view_id, pass_id = hg.SubmitSceneToPipeline(view_id, main_scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res, pipeline_aaa, pipeline_aaa_config, frame)
+	-- view_id, pass_id = hg.SubmitSceneToPipeline(view_id, main_scene, hg.IntRect(0, 0, res_x, res_y), true, pipeline, res)
 
 	frame = hg.Frame()
 	hg.UpdateWindow(win)
