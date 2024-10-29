c892d45bfad73483cba3bbaf2aa2b222b65750fe
fra <fra@movida-mail.com>
2024-10-29 15:37:19 +0100

    Variable dt.

--- app/main.lua
+++ app/main.lua
@@ -65,7 +65,7 @@ local miles_pos = {main_scene:GetNode("mile_0"):GetTransform():GetPos(), main_sc
 frame = 0
 
 while not hg.ReadKeyboard():Key(hg.K_Escape) and hg.IsWindowOpen(win) do
-	dt = hg.time_from_sec_f(1.0 / 60.0) -- hg.TickClock()
+	dt = hg.TickClock() -- hg.time_from_sec_f(1.0 / 60.0) -- hg.TickClock()
 
 	local view_id = 0
 	local pass_id
